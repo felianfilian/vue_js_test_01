337670d78d4a90d2a2ef2e6a70d50aec34d4a66c
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-10-19 15:36:10 +0200

    login form test

--- app.js
+++ app.js
@@ -10,4 +10,24 @@ let vueOne = new Vue({
       { name: "luigi", password: "abc" },
     ],
   },
+
+  methods: {
+    checkPassword(password) {
+      return this.users.filter((user) => password === user.password);
+    },
+    checkCredentials(event) {
+      event.preventDefault();
+      if (
+        this.username === this.currentUser &&
+        this.checkPassword(this.password).length !== 0
+      ) {
+        this.feedback = "good credentials";
+      } else {
+        this.feedback = "bad credentials";
+      }
+    },
+    changeUser(name) {
+      this.currentUser = name;
+    },
+  },
 });
